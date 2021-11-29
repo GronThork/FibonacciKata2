3beada8afd99a232ed281ee717597037fe53ba75
salvarez <salvarez@voxelgroup.net>
2021-11-29 16:01:32 +0100

    feat: for input one return one

--- FibonacciKata2/Fibonacci.cs
+++ FibonacciKata2/Fibonacci.cs
@@ -4,6 +4,10 @@ namespace FibonacciKata2
     {
         public static int Calculate(int i)
         {
+            if (i == 1)
+            {
+                return 1;
+            }
             return 0;
         }
     }
