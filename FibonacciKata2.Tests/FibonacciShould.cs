df5c83eb5578402d0eb551be4e911a21ed5b1ae3
salvarez <salvarez@voxelgroup.net>
2021-11-29 15:51:32 +0100

    chore: create test project

---
+++ FibonacciKata2.Tests/FibonacciShould.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace FibonacciKata2.Tests
+{
+    public class FibonacciShould
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
